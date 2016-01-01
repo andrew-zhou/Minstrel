29da4ca38b5ca1646631d2295bb333edaa7af9f5
Andrew Zhou <mrehzee@gmail.com>
2015-12-31 19:09:09 -0500

    Added Bootstrap CSS + grunt tasks for SASS to CSS conversion.

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -5,11 +5,39 @@ module.exports = function(grunt) {
 		browserify: {
 			'../public/app.js': ['app.js']
 		},
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					loadPath: 'node_modules/bootstrap-sass/assets/stylesheets'
+				},
+				files: {
+					'../public/style.css': 'style.scss'
+				},
+			},
+			dist: {
+				options: {
+					style: 'compressed',
+					loadPath: 'node_modules/bootstrap-sass/assets/stylesheets'
+				},
+				files: {
+					'../public/style.css': 'style.scss'
+				},
+			}
+		},
 		watch: {
-			files: ['**/*.js'],
-			tasks: ['browserify']
+			scripts: {
+				files: ['**/*.js'],
+				tasks: ['browserify']
+			},
+			styles: {
+				files: ['**/*.scss'],
+				tasks: ['sass:dev']
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('buildcss', ['sass:dist']);
 };
--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 $ = jQuery = require('jquery');
-var bootstrap = require('bootstrap');
+var bootstrap = require('bootstrap-sass');
 console.log('----------------- JQUERY --------------------');
 console.log(jQuery);
 console.log('----------------- BOOTSTRAP -------------------');
